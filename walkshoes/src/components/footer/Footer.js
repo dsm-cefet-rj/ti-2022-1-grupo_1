adffa7d0ab8ce976321fdffef645223305443b8b
rodrigomb13 <rodrigomb013@gmail.com>
2022-05-17 11:32:28 -0300

    componetização

---
+++ walkshoes/src/components/footer/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Footer.css"
+
+function footer() {
+  return (
+    <>
+        <footer>
+            <h1>Todos os Direitos Reservados ao Grupo 1 - PSW</h1>
+        </footer>
+    </>
+    
+  )
+}
+
+export default footer
---
+++ walkshoes/src/components/pags/pagamento/DadosPessoais.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import Menu from '../../header/Menu'
+import Footer from '../../footer/Footer'
+import "./styles.css"
+
+const DadosPessoais = () => {
+  return (
+    <>
+        <h2> FINALIZAR COMPRA</h2>
+            <div class="div-etapa-pessoal"> 
+                <span> DADOS PESSOAIS</span>
+            </div>
+            <div >
+                <form class="dados">
+                    <div>
+                        <label> Email </label>
+                        <input type="text" placeholder="Digite seu Email" name="email" required/> 
+
+                        <label> Nome </label>
+                        <input type="text" placeholder="Digite seu nome" name="nome" required/>
+
+                        <label> CPF </label>
+                        <input type="number" placeholder="Digite seu CPF" name="CPF" required/>
+
+                        <label> Telefone </label>
+                        <input type="number" placeholder="Digite seu telefone" name="telefone" required/>
+
+                        <button>IR PARA A ENTREGA</button>
+                    </div>
+                </form>
+            </div>
+    </>
+    
+  )
+}
+
+export default DadosPessoais
