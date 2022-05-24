8134fefd3325933e6ae3c1cc499fec51b9520d1d
devgabrieldesouza <wolfbotz.seguidor@gmail.com>
2022-05-24 17:27:05 -0300

    added(Hooks): Adicionei um esqueleto de Hooks ao componente List

--- walkshoes/src/components/list/List.js
+++ walkshoes/src/components/list/List.js
@@ -1,30 +1,48 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Card from '../../components/card/cardFavorito/CardFavorito'
 import './List.css'
 
 const List = () => {
 
+    const [search, setSearch] = useState("");
+    const toSearch = (s) => {
+        setSearch(s);
+        console.log("Pesquisando: " + s);
+    };
+
+    useEffect(() => {
+        
+    }, []);
+
   return (
     <>
         <div className="listWrapper">
             <div>
-                <div class="favLabel">
-                    <input type="text" name="search" placeholder="Pesquisar" />
+                <div className="favLabel">
+                    <input 
+                        type="text"
+                        name="search"
+                        placeholder="Pesquisar"
+                        onChange={(e) => toSearch(e.target.value)} 
+                    />
                     <button>Ordenar</button>
                 </div>
             </div>
 
             <div>
-                <div class="productLabel f1">
-                    <div style={{display: "flex", flexDirection: "column-reverse", alignItems: "center"}}>
-                        <input type="checkbox" name="select-all" />
-                        <label for="select-all">Todos</label>
+                <div className="productLabel f1">
+                    <div className="stack mid checkw">
+                        <input 
+                            type="checkbox" 
+                            name="select-all"
+                        />
+                        <label htmlFor="select-all">Todos</label>
                     </div>
                     <button style={{display: "flex", alignSelf: "center", alignItems: "center", height: "25px"}} disabled>Excluir</button>
                 </div>
             </div>
 
-            <div>
+            <div className='listy'>
                 <Card name="Nike Air-Max Axis Premium" price="200,00" situacao="Em estoque" />
                 <Card name="Nike Air-Max Axis Premium" price="200,00" situacao="Em estoque" />
                 <Card name="Nike Air-Max Axis Premium" price="200,00" situacao="Em estoque" />
