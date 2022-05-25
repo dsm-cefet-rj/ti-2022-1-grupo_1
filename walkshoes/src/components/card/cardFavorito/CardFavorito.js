bb64e810a806f34ffce2ea2ebf042c4d51c4499e
Lucas <58084632+sbohfm@users.noreply.github.com>
2022-05-25 11:12:19 -0300

    added(Hooks): Adicionei hooks as propriedades do card favoritos

--- walkshoes/src/components/card/cardFavorito/CardFavorito.js
+++ walkshoes/src/components/card/cardFavorito/CardFavorito.js
@@ -1,16 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import './CardFavorito.css'
-import imagemTenis from '../../../assets/nike-dunk.png'
-import priceIcon from '../../../assets/real-icon.png'
 
 const CardFavorito = (props) => {
 
     const navigate = useNavigate();
 
+    const data = props.item;
+
+    //States de propriedades do card
+    const [ img, setImage ] = useState(data.img);
+    const [ nome, setNome ] = useState(data.title);
+    const [ icon, setIcon ] = useState(data.icon);
+    const [ price, setPrice ] = useState(data.value);
+    const [ situation, setSituation ] = useState(data.situation);
+
+    //Efeito Colateral para atualizar estados (WIP)
+    useEffect(() => {
+        
+    }, []);
+
   return (
     <>
-        <div className="cardHand" onClick={() => navigate("/pagamento")}>
+        <div className="cardHand" onClick={() => navigate("/carrinho")}> {/* Devia ir pra pagina do produto na real */}
             <div className="productItem f1">
                 <div className="checkw">
                     <input type="checkbox" />
@@ -18,16 +30,16 @@ const CardFavorito = (props) => {
 
                 <div className="productContentInfo">
                     <div className="productContent">
-                        <img src={imagemTenis} />
+                        <img src={img} />
                     </div>
 
                     <div className="productInfo">
-                        <span style={{textTransform: "uppercase", fontWeight: "bold"}}>{props.name}</span>
+                        <span style={{textTransform: "uppercase", fontWeight: "bold"}}>{nome}</span>
                         
                         <div className="middle-h middle-v">
                             <div className="productPrice">
-                                <img src={priceIcon} />
-                                <span>{props.price}</span>
+                                <img src={icon} />
+                                <span>{price}</span>
                             </div>
                         </div>
                     </div>
@@ -35,7 +47,7 @@ const CardFavorito = (props) => {
 
                 <div className="situationInfo">
                     <div>
-                        {props.situacao}
+                        {situation}
                     </div>
                 </div>
             </div>
