4a684d30eeb40cd0d07516c7bcc85afe8c54cfdc
Larissa Alexandre <larissa.alexandre@g.globo>
2022-05-25 15:39:22 -0300

    Compartilhando estados entre o componente cardListHome e o CadastroProd + atualização do css

--- walkshoes/src/components/card/cardListHome/CardListHome.js
+++ walkshoes/src/components/card/cardListHome/CardListHome.js
@@ -1,57 +1,14 @@
-import React from "react";
 import CardHome from "../cardHome/CardHome";
 import "./CardListHome.css";
 import Search from "../../search/Search";
 
-export default function CardListHome() {
-  const cards = [
-    {
-      title: "Nike Air-Max Axis Premium",
-      type: "Masculino",
-      value: "650,00",
-    },
-    {
-      title: "Nike Air-Max Axis Premium",
-      type: "Feminino",
-      value: "950,00",
-    },
-    {
-      title: "Nike Air-Max Axis Premium",
-      type: "Esportivo",
-      value: "349,00",
-    },
-    {
-      title: "Nike Air-Max Axis Premium",
-      type: "Feminino",
-      value: "260,00",
-    },
-    {
-      title: "Nike Air-Max Axis Premium",
-      type: "Feminino",
-      value: "570,00",
-    },
-    {
-      title: "Nike Air-Max Axis Premium",
-      type: "Masculino",
-      value: "450,00",
-    },
-    {
-      title: "Nike Air-Max Axis Premium",
-      type: "Esportivo",
-      value: "600,00",
-    },
-    {
-      title: "Nike Air-Max Axis Premium",
-      type: "Feminino",
-      value: "950,00",
-    },
-  ];
-
+export default function CardListHome(props) {
+  
   return (
     <div className="contentlist">
       <Search></Search>
       <div className="list">
-        {cards.map((shoe) => (
+        {props.card.map((shoe) => (
           <CardHome card={shoe}></CardHome>
         ))}
       </div>
--- walkshoes/src/components/formProduto/form/Form.js
+++ walkshoes/src/components/formProduto/form/Form.js
@@ -1,18 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import IdentificProd from "../identificProd/IdentificProd";
 import InfoProd from "../infoProd/InfoProd";
 import "./Form.css";
 
 export default function Form(props){
 
+  const [card, setCard] = useState ({});
+
+  function handleUpdatedStatus(e){
+  setCard({...card, [e.target.name]: e.target.value});
+  }
+
+  function handleSubmit(e){
+    e.preventDefault();
+    props.setCards(props.cards.concat(card));
+    navigate('/')
+  }
+
+const navigate= useNavigate();
   return (
     <div className="formgeral">
         <h3 className="form_title">Cadastro de Produto</h3>
-      <form>
-        <IdentificProd></IdentificProd>
-        <InfoProd></InfoProd>
+      <form onSubmit={handleSubmit}>
+        <IdentificProd card={card} handleUpdatedStatus={handleUpdatedStatus} ></IdentificProd>
+        <InfoProd card={card} handleUpdatedStatus={handleUpdatedStatus} ></InfoProd>
         <div className="button">
-          <input className="input_submit" type="submit"></input>
+          <button className="input_submit" type="submit">Enviar</button>
         </div>
       </form>
     </div>
