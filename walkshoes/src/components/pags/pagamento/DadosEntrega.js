e5762812c0ee633747379815df124fffecbae2d2
rodrigomb13 <rodrigomb013@gmail.com>
2022-06-07 23:41:59 -0300

    melhorando o css e as navegações entre pag

--- walkshoes/src/components/pags/pagamento/DadosEntrega.js
+++ walkshoes/src/components/pags/pagamento/DadosEntrega.js
@@ -1,6 +1,10 @@
 import React, { useState } from "react";
 import "./styles.css";
 import { useNavigate } from "react-router-dom";
+import {BsFillPersonFill} from "react-icons/bs";
+import {AiFillCreditCard, AiOutlineBarcode} from "react-icons/ai"
+import {IoHomeSharp} from "react-icons/io5";
+import {FaMoneyCheckAlt, FaEdit} from "react-icons/fa"
 import Menu from "../../header/menu/Menu";
 import Footer from "../../footer/Footer";
 
@@ -24,6 +28,10 @@ function DadosEntrega() {
 
   const navigate = useNavigate();
 
+  function handleDadosPessoais(){
+    navigate("/dadospessoais")
+  }
+
   function handlePagamento() {
     navigate("/tipopagamento");
   }
@@ -33,42 +41,52 @@ function DadosEntrega() {
       <Menu name="Carrinho"></Menu>
       <div className="div-pagamento">
       <h2> FINALIZAR COMPRA</h2>
-      <div className="div-etapa-entrega">
-        <span> ENTREGA</span>
-      </div>
-      <div>
-        <form onSubmit={handleSubmit}>
-          <div className="dados">
-            <label> CEP </label>
-            <input
-              type="text"
-              placeholder="Digite seu CEP"
-              name="cep"
-              onChange={(event) => setCep(event.target.value)}
-            />
+      <div className="content-wr">
+          <div>
+            <form className="dados" onSubmit={handleSubmit}>
+              <div className="div-etapa-entrega">
+                <span> <IoHomeSharp/> ENTREGA</span>
+              </div>
+              <div>
+                <label> CEP </label>
+                <input
+                  type="text"
+                  placeholder="Digite seu CEP"
+                  name="cep"
+                  onChange={(event) => setCep(event.target.value)}
+                />
 
-            <label> Rua </label>
-            <input
-              type="text"
-              placeholder="Digite sua Rua"
-              name="rua"
-              onChange={(event) => setRua(event.target.value)}
-            />
+                <label> Rua </label>
+                <input
+                  type="text"
+                  placeholder="Digite sua Rua"
+                  name="rua"
+                  onChange={(event) => setRua(event.target.value)}
+                />
 
-            <label> Complemento </label>
-            <input
-              type="text"
-              placeholder="Digite o Complemento"
-              name="Complemento"
-              onChange={(event) => setComplemento(event.target.value)}
-            />
+                <label> Complemento </label>
+                <input
+                  type="text"
+                  placeholder="Digite o Complemento"
+                  name="Complemento"
+                  onChange={(event) => setComplemento(event.target.value)}
+                />
 
-            <button className="btn-proxima" type="submit" value="pagamento">
-              IR PARA PAGAMENTO
-            </button>
+                <button className="btn-proxima" type="submit" value="pagamento">
+                  IR PARA PAGAMENTO
+                </button>
+              </div>
+            </form>
           </div>
-        </form>
-      </div>
+          <div className="card-entrega">
+            <span><BsFillPersonFill/> DADOS PESSOAIS <button type="button" onClick={handleDadosPessoais}> <FaEdit/></button></span> 
+            <h5>Editar dados pessoais!</h5>
+          </div>
+          <div className="card-entrega">
+            <span> <AiFillCreditCard/> PAGAMENTO </span> 
+            <h5>Aguardando o preenchimento dos dados!</h5>
+          </div>
+        </div>
       </div>
       <Footer />
     </>
--- walkshoes/src/components/pags/pagamento/DadosPessoais.js
+++ walkshoes/src/components/pags/pagamento/DadosPessoais.js
@@ -1,5 +1,10 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import {BsFillPersonFill} from "react-icons/bs";
+import {AiFillCreditCard, AiOutlineBarcode} from "react-icons/ai"
+import {IoHomeSharp} from "react-icons/io5";
+import {FaMoneyCheckAlt, FaEdit} from "react-icons/fa"
+import Chat from "../../chat/Chat";
 import Menu from "../../header/menu/Menu";
 import Footer from "../../footer/Footer";
 import "./styles.css";
@@ -22,8 +27,8 @@ const DadosPessoais = () => {
     }
   }
 
-  function handleEntrega() {
-    navigate("/dadosentrega");
+  function handleEntrega(){
+    navigate("/dadosentrega")
   }
 
   return (
@@ -31,51 +36,62 @@ const DadosPessoais = () => {
       <Menu name="Carrinho"></Menu>
       <div className="div-pagamento">
         <h2> FINALIZAR COMPRA</h2>
-        <div className="div-etapa-pessoal">
-          <span> DADOS PESSOAIS</span>
-        </div>
-        <div>
-          <form className="dados" onSubmit={handleSubmit}>
-            <div>
-              <label> Email </label>
-              <input
-                type="text"
-                placeholder="Digite seu Email"
-                name="email"
-                onChange={(event) => setEmail(event.target.value)}
-              />
+        <div className="content-wr">
+          <div>
+            <form className="dados" onSubmit={handleSubmit}>
+              <div className="div-etapa-pessoal">
+                <span> <BsFillPersonFill/> DADOS PESSOAIS</span>
+              </div>
+              <div>
+                <label> Email </label>
+                <input
+                  type="text"
+                  placeholder="Digite seu Email"
+                  name="email"
+                  onChange={(event) => setEmail(event.target.value)}
+                />
 
-              <label> Nome </label>
-              <input
-                type="text"
-                placeholder="Digite seu nome"
-                name="nome"
-                onChange={(event) => setNome(event.target.value)}
-              />
+                <label> Nome </label>
+                <input
+                  type="text"
+                  placeholder="Digite seu nome"
+                  name="nome"
+                  onChange={(event) => setNome(event.target.value)}
+                />
 
-              <label> CPF </label>
-              <input
-                type="number"
-                placeholder="Digite seu CPF"
-                name="CPF"
-                onChange={(event) => setCpf(event.target.value)}
-              />
+                <label> CPF </label>
+                <input
+                  type="number"
+                  placeholder="Digite seu CPF"
+                  name="CPF"
+                  onChange={(event) => setCpf(event.target.value)}
+                />
 
-              <label> Telefone </label>
-              <input
-                type="number"
-                placeholder="Digite seu telefone"
-                name="telefone"
-                onChange={(event) => setTelefone(event.target.value)}
-              />
+                <label> Telefone </label>
+                <input
+                  type="number"
+                  placeholder="Digite seu telefone"
+                  name="telefone"
+                  onChange={(event) => setTelefone(event.target.value)}
+                />
 
-              <button className="btn-proxima" type="submit" value="entrega">
-                IR PARA A ENTREGA
-              </button>
-            </div>
-          </form>
+                <button className="btn-proxima" type="submit" value="entrega">
+                  IR PARA A ENTREGA
+                </button>
+              </div>
+            </form>
+          </div>
+          <div className="card-entrega">
+              <span> <IoHomeSharp/> DADOS ENTREGA </span> 
+              <h5>Aguardando o preenchimento dos dados!</h5>
+          </div>
+          <div className="card-entrega">
+            <span> <AiFillCreditCard/> PAGAMENTO </span> 
+            <h5>Aguardando o preenchimento dos dados!</h5>
+          </div>
         </div>
       </div>
+      <Chat/>
       <Footer />
     </>
   );
