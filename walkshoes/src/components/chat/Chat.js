7ef3de6ac217476582414d39493590bc0e0593bf
Lucas <58084632+sbohfm@users.noreply.github.com>
2022-06-08 12:16:03 -0300

    fix: className console error
    Bagulho funciona com class, mas fica reclamando q e pra botar className, vai entender

--- walkshoes/src/components/chat/Chat.js
+++ walkshoes/src/components/chat/Chat.js
@@ -14,29 +14,29 @@ const Chat = () => {
   return (
     <>
       <input type="checkbox" id="click"/>
-              <label className="chat-label" for="click" onClick={scrollUp}>
+              <label className="chat-label" htmlFor="click" onClick={scrollUp}>
                   <i> <FontAwesomeIcon icon="fa-solid fa-message" /></i>
-                  <i class="fas fa-times"></i>
+                  <i className="fas fa-times"></i>
               </label>
-            <div class="wrapper">
-                <div class="head-text">
+            <div className="wrapper">
+                <div className="head-text">
                     Tem um tenis para vender e está em boas condições? Chama nós!
                 </div>
-                <div class="chat-box">
-                    <div class="desc-text">
+                <div className="chat-box">
+                    <div className="desc-text">
                     Preencha seus Dados abaixo
                     </div>
                     <form action="#">
-                    <div class="field">
+                    <div className="field">
                         <input type="text" placeholder="Nome" required/>
                     </div>
-                    <div class="field">
+                    <div className="field">
                         <input type="email" placeholder="Email" required/>
                     </div>
-                    <div class="field textarea">
+                    <div className="field textarea">
                         <textarea cols="20" rows="10" placeholder="Conta mais sobre o tênis" required></textarea>
                     </div>
-                    <div class="field">
+                    <div className="field">
                         <button type="submit">Iniciar Chat</button>
                     </div>
                     </form>
