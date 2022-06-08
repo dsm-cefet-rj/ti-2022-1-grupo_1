8a05f46465a2d23ec5014546961393281704b6a9
Lucas <58084632+sbohfm@users.noreply.github.com>
2022-06-08 18:01:05 -0300

    feat: Forms slice na redux store

--- walkshoes/src/store/localStorage.js
+++ walkshoes/src/store/localStorage.js
@@ -1,4 +1,11 @@
-export const loadState = () => {
+export const saveState = (state) => {   //Guarda store no localStorage
+    try {
+        const serializedState = JSON.stringify(state);
+        localStorage.setItem('state', serializedState);
+    } catch {}
+};
+
+export const loadState = () => {    //Recupera store do localStorage
     try {
         const serializedState = localStorage.getItem('state');
         if (serializedState === null) {
@@ -9,25 +16,19 @@ export const loadState = () => {
         return undefined;
     }
 };
-export const saveState = (state) => {
-    try {
-        const serializedState = JSON.stringify(state);
-        localStorage.setItem('state', serializedState);
-    } catch {
-        // ignore write errors
-    }
-};
 
-window.onbeforeunload = function (e) {
+
+//Limpa localStorage
+window.onbeforeunload = function (e) {  //Salva o horario que a pagina foi fechada
     window.localStorage.unloadTime = JSON.stringify(new Date());
 };
     
-window.onload = function () {
+window.onload = function () {           //Compara os horarios do fechamento e reabertura e remove state
     let loadTime = new Date();
     let unloadTime = new Date(JSON.parse(window.localStorage.unloadTime));
     let refresh = loadTime.getTime() - unloadTime.getTime();
     
-    if(refresh>5000){
+    if(refresh>=3500){
         window.localStorage.removeItem("state");
     }
 };
--- walkshoes/src/store/store.js
+++ walkshoes/src/store/store.js
@@ -2,10 +2,12 @@ import { configureStore } from '@reduxjs/toolkit';
 import { loadState } from './localStorage.js';
 
 import favReducer from './favSlice';
+import formReducer from './formSlice';
 
 export default configureStore({
     reducer: {
         fav: favReducer,
+        form: formReducer,
     },
     preloadedState: loadState(),
 });
