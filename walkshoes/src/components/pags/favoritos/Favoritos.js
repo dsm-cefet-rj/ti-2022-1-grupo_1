1c745ee5e86785a716463c59bbe2c6281e58cbfe
Lucas <58084632+sbohfm@users.noreply.github.com>
2022-06-10 22:13:25 -0300

    ops(Favoritos): Comentários

--- walkshoes/src/components/pags/favoritos/Favoritos.js
+++ walkshoes/src/components/pags/favoritos/Favoritos.js
@@ -1,10 +1,14 @@
+//React
 import React from 'react'
+
+//Components
 import Wrapper from '../../favoritosWrapper/FavoritosWrapper'
 import Menu from '../../../components/header/menu/Menu'
 import Footer from '../../../components/footer/Footer'
 import Chat from "../../chat/Chat";
 
 const Favoritos = () => {
+  //HTML
   return (
     <>
       <Menu name="Carrinho"></Menu>
