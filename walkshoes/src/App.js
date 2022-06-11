857942193edf7267f1d0cf77693b411f9e028c88
Lucas <58084632+sbohfm@users.noreply.github.com>
2022-06-10 22:07:53 -0300

    ops(App): Refatoração e comentários

--- walkshoes/src/App.js
+++ walkshoes/src/App.js
@@ -1,33 +1,43 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import "./App.css";
+// [React]
 import React, {useState, useEffect} from "react";
 import { Provider } from 'react-redux';
-import "./components/FontawsomeIcons";
-import DadosPessoais from "./components/pags/pagamento/DadosPessoais";
-import DadosEntrega from "./components/pags/pagamento/DadosEntrega";
-import Footer from "./components/footer/Footer";
-import TipoPagamento from "./components/pags/pagamento/TipoPagamento";
-import Pagamento from "./components/pags/pagamento/Pagamento";
-import Home from "./components/pags/home/Home";
-import Carrinho from "./components/pags/carrinho/Carrinho";
-import Favoritos from "./components/pags/favoritos/Favoritos";
-import CadastroProd from "./components/pags/produto/cadastro/CadastroProd";
+
+// [Router]
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// [Assets]
 import tenis1 from "./assets/adidas1.jpeg"
 import tenis2 from "./assets/nike2.jpg"
 import tenis3 from "./assets/nike3.jpg"
 import tenis4 from "./assets/nike4.jpg"
 import tenis5 from "./assets/puma1.jpg"
-import footer from "./components/footer/Footer";
-import InfoProd from "./components/pags/infoProd/InfoProd";
+import Footer from "./components/footer/Footer";
 
+// [Componentes]
+import "./components/FontawsomeIcons";
+import Home from "./components/pags/home/Home";
+import DadosPessoais from "./components/pags/pagamento/DadosPessoais";
+import DadosEntrega from "./components/pags/pagamento/DadosEntrega";
+import TipoPagamento from "./components/pags/pagamento/TipoPagamento";
+import Pagamento from "./components/pags/pagamento/Pagamento";
+import Carrinho from "./components/pags/carrinho/Carrinho";
+import InfoProd from "./components/pags/infoProd/InfoProd";
+import Favoritos from "./components/pags/favoritos/Favoritos";
+import CadastroProd from "./components/pags/produto/cadastro/CadastroProd";
+
+// [Redux]: store e implementacao do LocalStorage
 import { saveState } from './store/localStorage.js';
 import debounce from 'lodash.debounce';
 import store from './store/store.js';
+
+// [CSS]
+import "./App.css";
 
 function App() {
 
   const [cards, setCards] = useState([]);
 
+  // Fetch do bd mockado
   useEffect(() => {
     fetch('http://localhost:3000/tenis')
     .then(res=>{
@@ -38,12 +48,14 @@ function App() {
     })
   }, []);
 
+  // Parte da implementacao do LocalStorage
   store.subscribe(
     debounce(() => {
       saveState(store.getState());
     }, 1000)
   );
 
+  // [HTML]
   return (
     <Provider store={store}>
       <Router>
