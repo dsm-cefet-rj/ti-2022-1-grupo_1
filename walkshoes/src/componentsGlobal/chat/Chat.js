78139a699348b04558e8814fed74c15eddea85a2
Lucas Jorge <lucas.jorge@reserve.com.br>
2022-06-15 12:33:55 -0300

    inserindo mensagens do chat no db.json via post request

--- walkshoes/src/componentsGlobal/chat/Chat.js
+++ walkshoes/src/componentsGlobal/chat/Chat.js
@@ -1,16 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import './Chat.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
+const Chat = () => {
+
+    const [isPending, setIsPending] = useState(false);
+    
     const scrollUp = () =>{
         window.scrollTo({
             top: 0,
             behavior: "smooth"
         })
     };
+    
+   const handleSubmit = (e) => {
+       const chat = [];
+       e.preventDefault();
+        setIsPending(true);
+        chat.push({
+            "nome": e.target.form[0].value,
+            "email": e.target.form[1].value,
+            "text": e.target.form[2].value,
+            "id": 1,
+            "idUsuario": 1
+           }) ;
+       
+       fetch('http://localhost:3000/chat', {
+           method: 'POST',
+           headers: { "Content-Type": "application/json"},
+           body: JSON.stringify(chat)
+       }).then(() =>{
+            console.log("Sucess");
+            setIsPending(false);
+       })
+   }
 
-
-const Chat = () => {
   return (
     <>
       <input type="checkbox" id="click"/>
@@ -27,18 +51,19 @@ const Chat = () => {
                     Preencha seus Dados abaixo
                     </div>
                     <form action="#">
-                    <div className="field">
-                        <input type="text" placeholder="Nome" required/>
-                    </div>
-                    <div className="field">
-                        <input type="email" placeholder="Email" required/>
-                    </div>
-                    <div className="field textarea">
-                        <textarea cols="20" rows="10" placeholder="Conta mais sobre o tênis" required></textarea>
-                    </div>
-                    <div className="field">
-                        <button type="submit">Iniciar Chat</button>
-                    </div>
+                        <div className="field">
+                            <input  type="text" placeholder="Nome" required/>
+                        </div>
+                        <div className="field">
+                            <input type="email" placeholder="Email" required/>
+                        </div>
+                        <div className="field textarea">
+                            <textarea cols="20" rows="10" placeholder="Conta mais sobre o tênis" required></textarea>
+                        </div>
+                        <div className="field">
+                            { !isPending && <button onClick={handleSubmit} type="submit">Enviar</button>}
+                            { isPending && <button onClick={handleSubmit} type="submit">Enviando Chat ...</button>}
+                        </div>
                     </form>
                 </div>
             </div>
