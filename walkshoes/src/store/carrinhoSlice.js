c6573cdaf395347d18214a8e94a71983fc28c4f1
Daniel Motta <danielmotta04@hotmail.com>
2022-06-22 03:25:22 -0300

    backend, mas com problema de preenchimento do array do db, as info vem vazia

--- walkshoes/src/store/carrinhoSlice.js
+++ walkshoes/src/store/carrinhoSlice.js
@@ -1,21 +1,63 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createEntityAdapter, createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { httpDelete, httpGet, httpPut, httpPost} from '../utils'
+
+const carAdapter = createEntityAdapter();
+
+export const fetchData = createAsyncThunk(
+    'car/fetchCard',
+    async () => {
+        const cards = await httpGet(`http://localhost:3000/carrinho`, 500);
+        return cards ? (cards.map((item) => ({...item}))) : (cards);
+    }
+);
+
+export const postCard = createAsyncThunk(
+    'car/postCard',
+    async (item) => {
+        const card = await httpPost(`http://localhost:3000/carrinho`, item, 500);
+        return {...card};
+    }
+);
+
+export const deleteCard = createAsyncThunk(
+    'car/removeCard',
+    async (item) => {
+        await httpDelete(`http://localhost:3000/carrinho/${item.id}`, 500);
+        return item.id;
+    }
+);
 
 export const slice = createSlice({
     name: 'carrinho',
-    initialState: {
-        data: [],
-    },
+    initialState: carAdapter.getInitialState({
+        loading: 'idle',
+        fetch: 'ready'
+    }),
     reducers: {
         createCardCar: (state, action) => {
             state.data = [action.payload].concat(state.data)
         },
-        deleteCard: (state, action) => {
-            state.data = state.data.filter((card) => card.id !== action.payload)
-        },
     },
+    extraReducers: {
+        
+        [fetchData.pending]: (state, action) => {state.loading = "loading"; state.fetch = "waiting";},
+        [fetchData.fulfilled]: (state, action) => {state.loading = "done"; state.fetch = "up-to-date"; carAdapter.addMany(state,action.payload);},
+        [fetchData.rejected]: (state, action) => {state.loading = "failed"; state.fetch = "ready";},
+
+        [postCard.pending]: (state, action) => {state.loading = "loading"; state.fetch = "waiting";},
+        [postCard.fulfilled]: (state, action) => {state.loading = "done"; state.fetch = "ready"; carAdapter.addOne(state,action.payload);},
+        [postCard.rejected]: (state, action) => {state.loading = "failed"; state.fetch = "up-to-date";},
+        
+        [deleteCard.pending]: (state, action) => {state.loading = "loading"; state.fetch = "waiting";},
+        [deleteCard.fulfilled]: (state, action) => {state.loading = "done"; state.fetch = "ready"; carAdapter.removeOne(state,action.payload);},
+        [deleteCard.rejected]: (state, action) => {state.loading = "failed"; state.fetch = "up-to-date";},
+
+    }
 })
 
-export const { createCardCar, deleteCard } = slice.actions;
-export const selectCards = state => state.carrinho.data;
+export const { createCardCar } = slice.actions;
+export const {
+    selectAll: selectAllData,
+} = carAdapter.getSelectors(state => state.carrinho);
 
 export default slice.reducer;
