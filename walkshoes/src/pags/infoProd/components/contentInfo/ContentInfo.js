6f87f068e9d614da6b2461df32cec2945381ddf4
Lucas <58084632+sbohfm@users.noreply.github.com>
2022-06-22 11:21:42 -0300

    wip(path): comunicacao entre home, prod, car e fav

--- walkshoes/src/pags/infoProd/components/contentInfo/ContentInfo.js
+++ walkshoes/src/pags/infoProd/components/contentInfo/ContentInfo.js
@@ -1,14 +1,37 @@
+import { useEffect } from 'react';
 import ProdImg from "../prodImg/ProdImg";
 import ProdInfo from "../prodInfo/ProdInfo";
+import { fetchData, selectCardsById } from '../../../../store/dataSlice.js';
+import { useSelector, useDispatch } from 'react-redux'
+import { useParams } from 'react-router-dom'
 import './ContentInfo.css';
 
 
 export default function ContentInfo() {
   
+  let { id } = useParams();
+  const content = useSelector((state) => selectCardsById(state, id));
+  const fetch = useSelector(state => state.data.loading);
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if(fetch !== 'done') {
+      dispatch(fetchData());
+  }
+  }, [])
+  
   return (
     <div className="contentinfo">
-      <ProdImg className="prodimg"></ProdImg>
-      <ProdInfo className="prodinfo"></ProdInfo>
+      {(fetch == 'done') ? (
+        <>
+          <ProdImg className="prodimg" content={content}></ProdImg>
+          <ProdInfo className="prodinfo" content={content}></ProdInfo>
+        </>
+        ) : (
+        <>
+          <p>Carregando...</p>
+        </>
+        )}
     </div>
   );
 }
--- walkshoes/src/pags/infoProd/components/prodImg/ProdImg.js
+++ walkshoes/src/pags/infoProd/components/prodImg/ProdImg.js
@@ -1,11 +1,11 @@
 import img from '../../../../assets/nike1.jpg'
 import './ProdImg.css';
 
-export default function ProdImg() {
+export default function ProdImg(props) {
   
     return (
       <div className="prodimgs">
-         <img className="shoes" src={img} />
+         <img className="shoes" src={props.content.img} />
       </div>
     );
   }
