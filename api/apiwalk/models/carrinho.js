c734d745f479aa35fef54aabbaf776d7a0d3af45
Daniel Motta <danielmotta04@hotmail.com>
2022-06-29 14:29:00 -0300

    inicio do mongodb pro carrinho, criacao do models carrinho

---
+++ api/apiwalk/models/carrinho.js
@@ -0,0 +1,15 @@
+// const moongose = require('mongoose')
+// const Schema = moongose.Schema
+
+// const carrinhoSchema = new Schema({
+//   id: Number,
+//   nome: String,
+//   valor: Number,
+//   tamanho: Number,
+//   estado: String,
+//   categoria: String,
+//   quantidade: Number,
+//   img: String
+// })
+
+// module.exports = carrinhoSchema
