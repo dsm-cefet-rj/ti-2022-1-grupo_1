fa1a358868695dd9a095299bade733f0cb4d6db6
Lucas Jorge <lucas.jorge@reserve.com.br>
2022-06-29 19:28:18 -0300

    implementado slice para o chat

--- walkshoes/src/store/store.js
+++ walkshoes/src/store/store.js
@@ -4,12 +4,14 @@ import { loadState } from './localStorage';
 import dataReducer from './dataSlice';
 import favReducer from './favSlice';
 import carrinhoReducer from './carrinhoSlice';
+import chatReducer from "./chatSlice";
 
 export default configureStore({
     reducer: {
         data: dataReducer,
         fav: favReducer,
         carrinho: carrinhoReducer,
+        msg: chatReducer
     },
     // preloadedState: loadState(),
 });
