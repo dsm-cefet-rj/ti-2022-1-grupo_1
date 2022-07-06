add7a6eaedb4b44156691ee526fa9627768074ba
Larissa Alexandre <larissa.alexandre@g.globo>
2022-07-06 16:38:15 -0300

    Tela de login

--- walkshoes/src/pags/produto/cadastro/CadastroProd.js
+++ walkshoes/src/pags/produto/cadastro/CadastroProd.js
@@ -3,12 +3,12 @@ import Menu from "../../../componentsGlobal/header/menu/Menu";
 import Footer from "../../../componentsGlobal/footer/Footer";
 import Form from "./components/form/Form";
 
-const CadastroProd = (props) => {
+const CadastroProd = () => {
   
   return (
     <>
       <Menu name="Carrinho"></Menu>
-      <Form cards={props.cards} setCards={props.setCards}></Form>
+      <Form></Form>
       <Footer></Footer>
     </>
   );
