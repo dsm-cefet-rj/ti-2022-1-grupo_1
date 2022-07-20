6875591aa23975794d53e5e9a1076d3630d6f6f6
Lucas <58084632+sbohfm@users.noreply.github.com>
2022-07-20 10:24:54 -0300

    wip(Passport): melhorias no request fav

--- api/apiwalk/routes/favoritos.js
+++ api/apiwalk/routes/favoritos.js
@@ -1,38 +1,59 @@
-var favoritos = require('../models/favoritosModel');
+var Favoritos = require('../models/favoritosModel');
 var express = require('express');
 const { isObjectIdOrHexString } = require('mongoose');
 var router = express.Router();
 
-/* GET users listing. */
 router.get('/', async function(req, res, next) {
-  let data = await favoritos.find({});
+  let id = req.user;
+  
+  await
+  (
+    (await Favoritos.findOne({user: id})) == null ?
+      Favoritos.create({user: id, ids: []})
+      : {}
+  );
 
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'application/json');
-  res.send(data);
+    let data = await Favoritos.findOne({user: id});
+    let items = await Tenis.find({id: data.ids});
+
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.send(items);
 });
 
 router.post('/add', async function(req, res, next){
-  let card = req.body;
-  
-  favoritos.create(card, (err, res) => {
-    if(err) return res.send(500, err);
-    console.log('Criou');
-  });
+  let id = req.user;
 
+  await
+  (
+    Favoritos.findOne({user: id}) == null ?
+      Favoritos.create({user: id, ids: []})
+      : {}
+  );
+
+  let card = req.body;
+
+  await Favoritos.updateOne(
+    {user: id},
+    {$push: {ids: card.id}}
+  );
+
+  res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
   res.send(card);
 });
 
-router.delete('/delete/:id', async function(req, res, next){
-  let id = req.params.id
-  favoritos.deleteOne({id: id}, (err, res) => {
-    if(err) return res.send(500, err);
-    console.log('Sumiu');
-  })
+router.delete('/delete/:array', async function(req, res, next){
+  let id = req.user;
+  let ids = req.params.array.split('&').map(Number);
+        
+  await Favoritos.updateOne(
+    {user: id},
+    {$pull: {ids: {$in: ids}}}
+  );
 
   res.statusCode = 200;
-  res.send(id);
+  res.send(ids);
 });
 
 module.exports = router;
